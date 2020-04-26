085f7c099f431182cdf44c5e54dc3d208783ad29
Robert Pym <rob_pym@hotmail.com>
2020-04-26 12:14:25 +0100

    Initial validation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
-
-// @route   GET api/users
-// @desc    Test route
+// JSON validation - see https://express-validator.github.io/docs/
+const { check, validationResult } = require('express-validator');
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('User test route'));
+router.post(
+  '/',
+  [
+    // How do we get these nearer the model?
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Must be a valid email address').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }); // Constant for this?
+      console.log();
+    }
+    res.send('User test route');
+  }
+);
 
 module.exports = router;
