85bc6e62679d645687abf53bc7180bf90264aad4
Robert Pym <rob_pym@hotmail.com>
2020-04-26 11:47:31 +0100

    2 API routes configured - 2 to do, crashing with TypeError

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const APP_LISTEN_PORT = process.env.PORT || 5000;
-
 const express = require('express');
-const connectDatabase = require('./config/db');
-
 const app = express();
 
 // Connect database
+const connectDatabase = require('./config/db');
 connectDatabase();
+
+// Intercept GET on /
+app.get('/', (req, res) => res.send('API running'));
+
+// Define API routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+//app.use('/api/profile', require('./routes/api/profile'));
+//app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(APP_LISTEN_PORT, () =>
   console.log(`Server started on port ${APP_LISTEN_PORT}`)
 );
-
-// Intercept GET on /
-app.get('/', (req, res) => res.send('API running'));
