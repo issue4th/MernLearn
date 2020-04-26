071787434a085fb67edba6ea6561411ef9e74177
Robert Pym <rob_pym@hotmail.com>
2020-04-26 11:29:59 +0100

    Plumb in connectDatabase

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const APP_LISTEN_PORT = process.env.PORT || 5000;
 
 const express = require('express');
+const connectDatabase = require('./config/db');
+
 const app = express();
+
+// Connect database
+connectDatabase();
 
 app.listen(APP_LISTEN_PORT, () =>
   console.log(`Server started on port ${APP_LISTEN_PORT}`)
