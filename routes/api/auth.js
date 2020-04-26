42d6022848761a0a42e2c361ab30d412742862d9
Robert Pym <rob_pym@hotmail.com>
2020-04-26 14:28:48 +0100

    Return user JSON for authorised GET on api/auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,32 @@
 const express = require('express');
-const auth = require('../../middleware/auth');
 const router = express.Router();
+
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', auth, (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    return serverErrorResponse(res);
+  }
+});
+
+function successResponse(res, message) {
+  return res.status(200).send(message);
+}
+
+function errorResponse(res, httpStatus, errorList) {
+  res.status(httpStatus).json({ errors: errorList });
+}
+
+function serverErrorResponse(res) {
+  return errorResponse(res, 500, [{ msg: 'Server error' }]);
+}
 
 module.exports = router;
